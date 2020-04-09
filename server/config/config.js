8e233d2f0e63a9664f58e8ebb0d3a0b5ec4ebef0
Asgamboa12 <33297469+Asgamboa12@users.noreply.github.com>
2020-04-09 16:03:13 -0600

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://andres-gamboa:rAbzbend1OaH85v0@cluster0-hvvmp.mongodb.net/cafe?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
